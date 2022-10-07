f68a010ebf74f2e7b42481bb5cac90ab83386ca2
Михаил Косточкин <kostochkin2002@inbox.ru>
2022-10-07 16:05:17 +0700

    07.10

--- MME lab 1/MainWindow.xaml.cs
+++ MME lab 1/MainWindow.xaml.cs
@@ -25,6 +25,252 @@ namespace MME_lab_1
 			InitializeComponent();
 			ChangeMatrixSize(null, null);
 		}
+
+		private class Matrix
+		{
+			public double[,] array;
+			public MatrixType type;
+			public Matrix(double[,] array, MatrixType type)
+			{
+				this.array = array;
+				this.type = type;
+			}
+			private double? GetSaddlePoint()
+			{
+				for (int i = 0; i < array.GetLength(0); i++)
+				{
+					for (int j = 0; j < array.GetLength(1); j++)
+					{
+						if (IsMinInRow(i, j) && IsMaxInCol(i, j))
+							return array[i, j];
+					}
+				}
+				return null;
+			}
+			private bool IsMaxInCol(int i, int j)
+			{
+				for (int k = 0; k < array.GetLength(0); k++)
+				{
+					if (array[k, j] > array[i, j])
+						return false;
+				}
+				return true;
+			}
+			private bool IsMinInRow(int i, int j)
+			{
+				for (int k = 0; k < array.GetLength(1); k++)
+				{
+					if (array[i, k] < array[i, j])
+						return false;
+				}
+				return true;
+			}
+			private void LaplaceCritera() // Добавить результат
+			{
+				List<(double, int, int)> values = new List<(double, int, int)>();
+				int i, j;
+				for (i = 0; i < array.GetLength(0); i++)
+				{
+					double sum = 0;
+					for (j = 0; j < array.GetLength(1); j++)
+					{
+						sum += array[i, j];
+					}
+					values.Add((sum * (1.0 / array.GetLength(1)), i, j));
+				}
+				switch (type)
+				{
+					case MatrixType.WinType:
+						double maxReuslt = values.Max(t => t.Item1);
+						//var maxResult = values.OrderByDescending(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По критерию Лапласа: Решение: {maxResult.Item2 + 1}, значение {maxResult.Item1}");
+						break;
+					case MatrixType.LoseType:
+						double minReulst = values.Min(t => t.Item1);
+						//var minResult = values.OrderBy(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По критерию Лапласа: Решение: {minResult.Item2 + 1}, значение {minResult.Item1}");
+						break;
+					default:
+						break;
+				}
+			}
+
+			private void WaldCriteria() //Вывод результата
+			{
+				List<(double, int, int)> values = new List<(double, int, int)>();
+				int i, j;
+				switch (type)
+				{
+					case MatrixType.WinType:
+						for (i = 0; i < array.GetLength(0); i++)
+						{
+							double minRow = double.MaxValue;
+							int index = 0;
+							for (j = 0; j < array.GetLength(1); j++)
+							{
+								if (minRow > array[i, j])
+								{
+									minRow = array[i, j];
+									index = j;
+								}
+							}
+							values.Add((minRow, i, index));
+						}
+						var maxResult = values.Max(t => t.Item1);	//.OrderByDescending(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По критерию Вальда:  Решение: {maxResult.Item2 + 1}, значение {maxResult.Item1}");
+						break;
+					case MatrixType.LoseType:
+
+						for (i = 0; i < array.GetLength(0); i++)
+						{
+							double maxRow = double.MinValue;
+							int index = 0;
+							for (j = 0; j < array.GetLength(1); j++)
+							{
+								if (maxRow < array[i, j])
+								{
+									maxRow = array[i, j];
+									index = j;
+								}
+							}
+							values.Add((maxRow, i, j));
+						}
+						var minResult = values.Min(t => t.Item1);	//OrderBy(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По критерию Вальда: Решение: {minResult.Item2 + 1}, значение {minResult.Item1}");
+						break;
+					default:
+						break;
+				}
+			}
+
+			private void HurwitzCriteria(double alpha) //Вывод
+			{
+				List<(double, int, int)> values = new List<(double, int, int)>();
+				int i, j;
+				double minRow, maxRow;
+				switch (type)
+				{
+					case MatrixType.WinType:
+						for (i = 0; i < array.GetLength(0); i++)
+						{
+							minRow = int.MaxValue;
+							maxRow = int.MinValue;
+							for (j = 0; j < array.GetLength(1); j++)
+							{
+								if (minRow > array[i, j])
+									minRow = array[i, j];
+								if (maxRow < array[i, j]) maxRow = array[i, j];
+							}
+							values.Add((alpha * maxRow + (1 - alpha) * minRow, i, j));
+						}
+						var maxResult = values.Max(t => t.Item1); //.OrderByDescending(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По критерию Гурвица:  Решение: {maxResult.Item2 + 1} значение {maxResult.Item1}");
+						break;
+					case MatrixType.LoseType:
+						for (i = 0; i < array.GetLength(0); i++)
+						{
+							minRow = int.MaxValue;
+							maxRow = int.MinValue;
+							for (j = 0; j < array.GetLength(1); j++)
+							{
+								if (minRow > array[i, j])
+									minRow = array[i, j];
+								if (maxRow < array[i, j]) maxRow = array[i, j];
+							}
+							values.Add((alpha * minRow + (1 - alpha) * maxRow, i, j));
+						}
+						var minResult = values.Min(t => t.Item1);//.OrderBy(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По критерию Гурвица:  Решение: {minResult.Item2 + 1} значение {minResult.Item1}");
+						break;
+					default:
+						break;
+				}
+			}
+
+			private void OptimismCritera()
+			{
+				List<(double, int, int)> values = new List<(double, int, int)>();
+				int i, j;
+				switch (type)
+				{
+					case MatrixType.WinType:
+						for (i = 0; i < array.GetLength(0); i++)
+						{
+							double maxRow = double.MinValue;
+							int index = 0;
+							for (j = 0; j < array.GetLength(1); j++)
+							{
+								if (maxRow < array[i, j])
+								{
+									maxRow = array[i, j];
+									index = j;
+								}
+							}
+							values.Add((maxRow, i, j));
+						}
+						var maxResult = values.OrderByDescending(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По критерию оптимизма: Решение: {maxResult.Item2 + 1}, значение {maxResult.Item1}");
+						break;
+					case MatrixType.LoseType:
+						for (i = 0; i < array.GetLength(0); i++)
+						{
+							double minRow = double.MaxValue;
+							int index = 0;
+							for (j = 0; j < array.GetLength(1); j++)
+							{
+								if (minRow > array[i, j])
+								{
+									minRow = array[i, j];
+									index = j;
+								}
+							}
+							values.Add((minRow, i, index));
+						}
+						var minResult = values.OrderBy(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По критерию оптимизма:  Решение: {minResult.Item2 + 1}, значение {minResult.Item1}");
+						break;
+					default:
+						break;
+				}
+			}
+
+			private void BayesCriteria(double[] probs)
+			{
+				if (array.GetLength(0) != probs.Length)
+				{
+					throw new Exception("Кол-во вероятностей и строк не совпадает!");
+				}
+				if (probs.Sum() != 1)
+				{
+					throw new Exception("Сумма вероятностей не равна 1!");
+				}
+				List<(double, int, int)> values = new List<(double, int, int)>();
+				int i, j;
+				for (i = 0; i < array.GetLength(0); i++)
+				{
+					double sum = 0;
+					for (j = 0; j < array.GetLength(1); j++)
+					{
+						sum += array[i, j] * probs[j];
+					}
+					values.Add((sum, i, j));
+				}
+				switch (type)
+				{
+					case MatrixType.WinType:
+						var maxResult = values.OrderByDescending(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По байесовскому методу: Решение: {maxResult.Item2 + 1}, значение {maxResult.Item1}");
+						break;
+					case MatrixType.LoseType:
+						var minResult = values.OrderBy(t => t.Item1).ToList().First();
+						//Console.WriteLine($"По байесовскому методу: Решение: {minResult.Item2 + 1}, значение {minResult.Item1}");
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
 		enum MatrixType
 		{
 			WinType,
@@ -33,14 +279,20 @@ namespace MME_lab_1
 		}
 		private int matrixWidth;
 		private int matrixHeight;
-		private MatrixType matrixType;
-		//TextBlock[,] matrixInput;
+		TextBox[,] matrixInput;
+		TextBox[] paramsInput;
+
 		private void ChangeMatrixSize(object sender, RoutedEventArgs e)
 		{
 			try
 			{
 				matrixWidth = Convert.ToInt32(MatrixWidthInput.Text);
 				matrixHeight = Convert.ToInt32(MatrixHeightInput.Text);
+				if (matrixWidth < 1 || matrixHeight < 1)
+					throw new Exception("Недостаточный размер матрицы");
+				matrixInput = new TextBox[matrixHeight, matrixWidth];
+				paramsInput = new TextBox[matrixWidth];
+				
 
 				ChangeInputMatrix();
 				ChangeInputParams();
@@ -52,7 +304,24 @@ namespace MME_lab_1
 				return;
 			}
 		}
-
+		private MatrixType GetMatrixType()
+		{
+			string str = MatrixTypeInput.Text;
+			if (str == "П" || str == "п")
+				return MatrixType.LoseType;
+			else if (str == "В" || str == "в")
+				return MatrixType.WinType;
+			else
+				return MatrixType.ErrorType;
+		}
+		private double[,] GetMatrixValues()
+		{
+			double[,] array = new double[matrixHeight, matrixWidth];
+			for (int i = 0; i < matrixHeight; i++)
+				for (int j = 0; j < matrixWidth; j++)
+					array[i, j] = Convert.ToDouble(matrixInput[i, j]);
+			return array;
+		}
 		private void ChangeInputMatrix()
 		{
 			#region CreateGrid
@@ -64,14 +333,15 @@ namespace MME_lab_1
 				newGrid.RowDefinitions.Add(new RowDefinition());
 			#endregion
 			#region FillGrid
-			for (int i = 1; i <= matrixHeight; i++)
+			for (int i = 0; i < matrixHeight; i++)
 			{
-				for (int j = 1; j <= matrixWidth; j++)
+				for (int j = 0; j < matrixWidth; j++)
 				{
 					TextBox textBox = new TextBox();
+					matrixInput[i, j] = textBox;
 					textBox.TextAlignment = TextAlignment.Center;
-					Grid.SetColumn(textBox, j);
-					Grid.SetRow(textBox, i);
+					Grid.SetColumn(textBox, j + 1);
+					Grid.SetRow(textBox, i + 1);
 					newGrid.Children.Add(textBox);
 				}
 			}
@@ -125,6 +395,7 @@ namespace MME_lab_1
 				newGrid.Children.Add(textBlock);
 
 				TextBox textBox = new TextBox();
+				paramsInput[i] = textBox;
 				textBox.TextAlignment = TextAlignment.Center;
 				Grid.SetColumn(textBox, i);
 				Grid.SetRow(textBox, 1);
@@ -220,20 +491,18 @@ namespace MME_lab_1
 
 		private void GetSolution(object sender, RoutedEventArgs e)
 		{
-			matrixType = MatrixType.ErrorType;
-			switch (MatrixTypeInput.Text)
+			try
 			{
-				case "В":
-				case "в":
-					matrixType = MatrixType.WinType;
-					break;
-				case "П":
-				case "п":
-					matrixType = MatrixType.LoseType;
-					break;
-				default:
-					matrixType = MatrixType.ErrorType;
-					break;
+				double[,] array = GetMatrixValues();
+				MatrixType matrixType = GetMatrixType();
+				if (matrixType == MatrixType.ErrorType)
+					throw new Exception("Ошибка в типе матрицы");
+				Matrix matrix = new Matrix(array, matrixType);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
 			}
 		}
 	}
